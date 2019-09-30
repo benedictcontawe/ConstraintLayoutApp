2322d8385c895cfbacb1040ee4e17d152bf62300
Benedict <benedict.b.contawe@accenture.com>
2019-09-30 15:41:56 +0800

    Update with trigger button for change constraint set

--- app/src/main/java/com/example/constraintlayoutapp/MainActivity.java
+++ app/src/main/java/com/example/constraintlayoutapp/MainActivity.java
@@ -1,23 +1,21 @@
 package com.example.constraintlayoutapp;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
-
-import static androidx.core.util.Preconditions.checkArgument;
+import androidx.constraintlayout.widget.Guideline;
+import androidx.transition.TransitionManager;
 
 public class MainActivity extends AppCompatActivity {
 
-    ConstraintLayout descriptionContainer;
-    ImageView descriptionImage;
-    TextView description;
+    ConstraintLayout constraintLayoutMain;
+    Guideline constraintGuidelineVertical8, constraintGuidelineVertical9, constraintGuidelineHorizontal2, constraintGuidelineHorizontal8, constraintGuidelineHorizontal9;
+    View firstView, secondView, thirdView;
+    Button btnConstraint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,44 +23,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setInstanceId();
+
+        btnConstraint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animateConstraint();
+            }
+        });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        setConstraint();
-    }
-
-    @Deprecated //This is for sample_layout only
     private void  setInstanceId() {
-        descriptionContainer = (ConstraintLayout) findViewById(R.id.descriptionContainer);
-        descriptionImage = (ImageView) findViewById(R.id.descriptionImage);
-        description = (TextView) findViewById(R.id.description);
+        constraintLayoutMain = (ConstraintLayout) findViewById(R.id.constraint_layout_main);
+        constraintGuidelineVertical8 = (Guideline) findViewById(R.id.constraint_guideline_vertical_8);
+        constraintGuidelineVertical9 = (Guideline) findViewById(R.id.constraint_guideline_vertical_9);
+        constraintGuidelineHorizontal2 = (Guideline) findViewById(R.id.constraint_guideline_horizontal_2);
+        constraintGuidelineHorizontal8 = (Guideline) findViewById(R.id.constraint_guideline_horizontal_8);
+        constraintGuidelineHorizontal9 = (Guideline) findViewById(R.id.constraint_guideline_horizontal_9);
+        firstView = (View) findViewById(R.id.first_view);
+        secondView = (View) findViewById(R.id.second_view);
+        thirdView = (View) findViewById(R.id.third_view);
+        btnConstraint = (Button) findViewById(R.id.btnConstraint);
     }
 
-    @Deprecated //This is for sample_layout only
-    private void setConstraint() {
-        //descriptionContainer
-        //descriptionImage
-        //description
-
+    private void animateConstraint() {
+        TransitionManager.beginDelayedTransition(constraintLayoutMain);
         ConstraintSet constraintSet = new ConstraintSet();
-        constraintSet.clone(descriptionContainer);
-        constraintSet.clear(descriptionImage.getId(),ConstraintSet.START);
-        constraintSet.clear(description.getId(),ConstraintSet.END);
-        constraintSet.applyTo(descriptionContainer);
+        constraintSet.clone(constraintLayoutMain);
+        //Clear firstView constraintSet
+        constraintSet.clear(firstView.getId(),ConstraintSet.TOP);
+        constraintSet.clear(firstView.getId(),ConstraintSet.START);
+        constraintSet.clear(firstView.getId(),ConstraintSet.END);
+        constraintSet.clear(firstView.getId(),ConstraintSet.BOTTOM);
+        //Connect firstView constraintSet
+        constraintSet.connect(firstView.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP);
+        constraintSet.connect(firstView.getId(), ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START);
+        constraintSet.connect(firstView.getId(), ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END);
+        constraintSet.connect(firstView.getId(), ConstraintSet.BOTTOM, constraintGuidelineHorizontal2.getId(), ConstraintSet.BOTTOM);
+        //Set Visibility to Invisible secondView constraintSet
+        constraintSet.setVisibility(secondView.getId(),ConstraintSet.INVISIBLE);
+        //Clear thirdView constraintSet
+        constraintSet.clear(thirdView.getId(),ConstraintSet.TOP);
+        constraintSet.clear(thirdView.getId(),ConstraintSet.START);
+        constraintSet.clear(thirdView.getId(),ConstraintSet.END);
+        constraintSet.clear(thirdView.getId(),ConstraintSet.BOTTOM);
+
+        constraintSet.connect(thirdView.getId(), ConstraintSet.TOP, constraintGuidelineHorizontal8.getId(), ConstraintSet.TOP);
+        constraintSet.connect(thirdView.getId(), ConstraintSet.START, constraintGuidelineVertical8.getId(), ConstraintSet.START);
+        constraintSet.connect(thirdView.getId(), ConstraintSet.END, constraintGuidelineVertical9.getId(), ConstraintSet.END);
+        constraintSet.connect(thirdView.getId(), ConstraintSet.BOTTOM, constraintGuidelineHorizontal9.getId(), ConstraintSet.BOTTOM);
+
+        constraintSet.applyTo(constraintLayoutMain);
     }
 
-    private static boolean isRtl(Context context) {
-        checkArgument(context != null, "context must not be null");
 
-        boolean isRtl = false;
-        try {
-            isRtl = context.getResources().getConfiguration().
-                    getLayoutDirection() == View.LAYOUT_DIRECTION_RTL;
-        } catch (NoSuchMethodError e) {
-            Log.e(MainActivity.class.getSimpleName(),e.getMessage());
-        }
-        return isRtl;
-    }
 }
