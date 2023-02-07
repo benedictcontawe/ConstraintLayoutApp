b932fd3a788e98175897fda99120932cc289944a
Benedict Contawe <benedictcontawe@gmail.com>
2023-02-07 20:46:52 +0800

    Add Animations for FABs

--- app/src/main/java/com/example/constraintlayoutapp/MainActivity.java
+++ app/src/main/java/com/example/constraintlayoutapp/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.constraintlayoutapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -10,6 +9,8 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.example.constraintlayoutapp.databinding.MainBinder;
 
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final String TAG = MainActivity.class.getSimpleName();
     private MainBinder binder;
     private MainViewModel viewModel;
-    private ConstraintSet constraintSet;
+    private Animation rotationOpen, rotationClose, fromBottom, toBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +29,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         binder.setViewModel(viewModel);
         binder.setLifecycleOwner(this);
-        constraintSet = new ConstraintSet();
 
         binder.floatingActionButtons.floatingActionButtonAndroid.setOnClickListener(this::onClick);
         binder.floatingActionButtons.floatingActionButtonAndroid.setOnTouchListener(this::onTouch);
+
+        rotationOpen = AnimationUtils.loadAnimation(this, R.anim.rotate_open_animation);
+        rotationClose = AnimationUtils.loadAnimation(this, R.anim.rotate_close_animation);
+        fromBottom = AnimationUtils.loadAnimation(this, R.anim.from_bottom_animation);
+        toBottom = AnimationUtils.loadAnimation(this, R.anim.to_bottom_animation);
 
         binder.getViewModel().observeFABVisibility().observe(this, new Observer<Boolean>() {
             @Override
@@ -83,16 +88,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void showExtendedFAB() {
+        binder.floatingActionButtons.floatingActionButtonAndroid.startAnimation(rotationOpen);
+
         binder.floatingActionButtons.extendedFloatingActionButtonOne.show();
+        binder.floatingActionButtons.extendedFloatingActionButtonOne.startAnimation(fromBottom);
+
         binder.floatingActionButtons.extendedFloatingActionButtonTwo.show();
+        binder.floatingActionButtons.extendedFloatingActionButtonTwo.startAnimation(fromBottom);
+
         binder.floatingActionButtons.extendedFloatingActionButtonThree.show();
+        binder.floatingActionButtons.extendedFloatingActionButtonThree.startAnimation(fromBottom);
+
         binder.floatingActionButtons.extendedFloatingActionButtonFour.show();
+        binder.floatingActionButtons.extendedFloatingActionButtonFour.startAnimation(fromBottom);
     }
 
     private void hideExtendedFab() {
+        binder.floatingActionButtons.floatingActionButtonAndroid.startAnimation(rotationClose);
+
         binder.floatingActionButtons.extendedFloatingActionButtonOne.hide();
+        binder.floatingActionButtons.extendedFloatingActionButtonOne.startAnimation(toBottom);
+
         binder.floatingActionButtons.extendedFloatingActionButtonTwo.hide();
+        binder.floatingActionButtons.extendedFloatingActionButtonTwo.startAnimation(toBottom);
+
         binder.floatingActionButtons.extendedFloatingActionButtonThree.hide();
+        binder.floatingActionButtons.extendedFloatingActionButtonThree.startAnimation(toBottom);
+
         binder.floatingActionButtons.extendedFloatingActionButtonFour.hide();
+        binder.floatingActionButtons.extendedFloatingActionButtonFour.startAnimation(toBottom);
     }
 }
